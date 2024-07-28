56d2a8b56eb6ff75bdb06ce7bdd819226995e073
EscuderoDev <escuderodev@gmail.com>
2024-07-28 12:50:01 -0300

    feat: create documentation with swagger initied

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import "dotenv/config"
 import { userRouter } from "../src/routes/userRoutes"
 import { disciplineRouter } from "./routes/disciplineRoutes"
 import { postRouter } from "./routes/postRoutes"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
 
 export class App {
     private server: express.Application
@@ -13,6 +15,7 @@ export class App {
         this.server.use(userRouter)
         this.server.use(disciplineRouter)
         this.server.use(postRouter)
+        this.server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
         this.server.get("/", (req: express.Request, res: express.Response) => {
             res.status(200).json({message: "Bem vindo a minha API!"})
         })
@@ -22,3 +25,5 @@ export class App {
         return this.server
     }
 }
+
+//https://www.youtube.com/watch?v=WhFx2heoFrA
