d636f5c915be37bac66248017e8f214b5ed2c18f
EscuderoDev <escuderodev@gmail.com>
2024-07-30 20:54:21 -0300

    feat: api user swagger documentation created

--- swagger.ts
+++ swagger.ts
@@ -6,17 +6,52 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'API de Disciplinas',
+      title: 'API de Disciplinas e Usuários',
       version: '1.0.0',
-      description: 'Documentação da API de Disciplinas',
+      description: 'Documentação da API de Disciplinas e Usuários',
     },
     servers: [
       {
         url: 'http://localhost:3000',
       },
     ],
+    components: {
+      schemas: {
+        User: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'ID do usuário',
+            },
+            name: {
+              type: 'string',
+              description: 'Nome do usuário',
+            },
+            email: {
+              type: 'string',
+              description: 'Email do usuário',
+            },
+            password: {
+              type: 'string',
+              description: 'Senha do usuário',
+            },
+            createdAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Data de criação do usuário',
+            },
+            updatedAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Data de atualização do usuário',
+            },
+          },
+        },
+      },
+    },
   },
-  apis: ['./src/routes/disciplineRoutes.ts'], // Caminho para os arquivos onde a documentação está escrita
+  apis: ['./src/routes/disciplineRoutes.ts', './src/routes/userRoutes.ts'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
